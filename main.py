fe090c22a7c60f3327e7af9229d42897cf1c6ecf
Tornyi Dénes <tornyi.denes@gmail.com>
2013-04-25 23:16:20 +0200

    Added http body upload

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-"""
 
-__author__ = "Tornyi Dénes"
-__version__ = "1.0.1"
+
+"""
+JamCropxy
+
+If you have any advice, please write to us.
+- Google Code: https://code.google.com/p/jamcrop/
+"""
+
+
+__author__ = ["Dénes Tornyi", "Ádám Tajti"]
+__version__ = "1.1.0"
 
 
 KEY = '***REMOVED***'
 SECRET = '***REMOVED***'
 
 
+from poster.streaminghttp import register_openers
 import mimetypes
-import httplib2
 import urlparse
 import webapp2
+import urllib2
 import oauth2
 import urllib
+import os
 
 
 class Dropbox():
     def authorize(self):
-
-        """ Get a request token from the Dropbox """
-
         consumer = oauth2.Consumer(KEY, SECRET)
         client = oauth2.Client(consumer)
         response, content = client.request("https://api.dropbox.com/1/oauth/request_token", 'GET')
         return dict(urlparse.parse_qsl(content))
 
     def access(self, request_token):
-
-        """ Get an access token from the Dropbox
-        :param request_token: Recently got request token.
-        """
-
         token = oauth2.Token(request_token['oauth_token'], request_token['oauth_token_secret'])
         consumer = oauth2.Consumer(KEY, SECRET)
         client = oauth2.Client(consumer, token)
@@ -40,14 +43,6 @@ class Dropbox():
         return dict(urlparse.parse_qsl(content))
 
     def sign(self, url, token, method = 'PUT', parameters = None):
-
-        """ Sign the give URL with oauth signature
-        :param url: URL to sign
-        :param token: Dropbox token
-        :param method: HTTP method (POST, GET, etc.)
-        :param parameters: Additional parameters
-        """
-
         consumer = oauth2.Consumer(KEY, SECRET)
         request = oauth2.Request.from_consumer_and_token(consumer, token, method, url, parameters)
         request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)
@@ -56,9 +51,6 @@ class Dropbox():
 
 class AuthorizePage(webapp2.RequestHandler, Dropbox):
     def get(self):
-
-        """ Show authorize page with GET method """
-
         request_token = self.authorize()
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(urllib.urlencode(request_token))
@@ -67,9 +59,9 @@ class AuthorizePage(webapp2.RequestHandler, Dropbox):
 class AccessPage(webapp2.RequestHandler, Dropbox):
     def get(self):
 
-        """ Show access page with GET method
-        :urlparam oauth_token: Token requested by the authorize page
-        :urlparam oauth_token_secret: Secret requested by the authorize page
+        """ Get an access token by the request token
+        :get oauth_token: Token requested by the authorize page
+        :get oauth_token_secret: Secret requested by the authorize page
         """
 
         if self.request.get('oauth_token') and self.request.get('oauth_token_secret'):
@@ -81,31 +73,32 @@ class AccessPage(webapp2.RequestHandler, Dropbox):
             self.error(400)
 
 
-class UploadPage(webapp2.RequestHandler, Dropbox):
+class UploadPage(webapp2.RequestHandler, Dropbox, webapp2.Request):
     def post(self):
 
-        """ Show upload page with POST method
-        :urlparam oauth_token: Token requested by the authorize page
-        :urlparam oauth_token_secret: Secret requested by the authorize page
-        :urlparam body: File body for upload
-        :urlparam name: File name
+        """ Upload screenshot to the server
+        :http body: Body of the file
+        :get oauth_token: Token requested by the authorize page
+        :get oauth_token_secret: Secret requested by the authorize page
+        :get name: Name of the file
         """
 
-        print(mimetypes.guess_type(self.request.get('name'))[0])
-
-        if self.request.get('oauth_token') and self.request.get('oauth_token_secret') and self.request.get('body') and \
+        if self.request.get('oauth_token') and self.request.get('oauth_token_secret') and \
            self.request.get('name') and (mimetypes.guess_type(self.request.get('name'))[0] == 'image/jpeg' or
-                                            mimetypes.guess_type(self.request.get('name'))[0] == 'image/pjpeg' or
-                                                    mimetypes.guess_type(self.request.get('name'))[0] == 'image/png'):
+                                         mimetypes.guess_type(self.request.get('name'))[0] == 'image/png'):
             token = oauth2.Token(self.request.get('oauth_token'), self.request.get('oauth_token_secret'))
-            headers = {'content-type': mimetypes.guess_type(self.request.get('name'))[0],
-                       'content-length': str(len(self.request.get('body')))}
             url = self.sign("https://api-content.dropbox.com/1/files_put/sandbox/%s" %
                             self.request.get('name'), token, 'PUT')
-            resp, content = httplib2.Http().request(url, 'PUT', body = self.request.get('body'), headers = headers)
+            headers = {'content-type': mimetypes.guess_type(self.request.get('name'))[0],
+                       'content-length': self.response.headers["content-length"]}
+
+            opener = register_openers()
+            request = urllib2.Request(url, os.environ['wsgi.input'], headers)
+            request.get_method = lambda: 'PUT'
+            content = opener.open(request)
 
             self.response.headers['Content-Type'] = 'text/plain'
-            self.response.write(content)
+            self.response.write(content.read())
         else:
             self.error(400)
 
@@ -113,23 +106,26 @@ class UploadPage(webapp2.RequestHandler, Dropbox):
 class SharePage(webapp2.RequestHandler, Dropbox):
     def get(self):
 
-        """ Show share page with GET method
-        :urlparam oauth_token: Token requested by the authorize page
-        :urlparam oauth_token_secret: Secret requested by the authorize page
-        :urlparam short: Length of the URL (if it is false, it will be long)
-        :urlparam name: File name
+        """ Get the uploaded screenshot URL
+        :get oauth_token: Token requested by the authorize page
+        :get oauth_token_secret: Secret requested by the authorize page
+        :get short: Length of the URL (if it is false, it will be long)
+        :get name: Name of the file
         """
 
         if self.request.get('oauth_token') and self.request.get('oauth_token_secret') and \
            self.request.get('short') and self.request.get('name'):
             token = oauth2.Token(self.request.get('oauth_token'), self.request.get('oauth_token_secret'))
-            url = self.sign("https://api.dropbox.com/1/shares/sandbox/%s?%s" %
-                            (self.request.get('name'),
+            url = self.sign("https://api.dropbox.com/1/shares/sandbox/%s?%s" % (self.request.get('name'),
                              urllib.urlencode({'short_url': self.request.get('short')})), token, 'POST')
-            resp, content = httplib2.Http().request(url, 'POST')
+
+            opener = register_openers()
+            request = urllib2.Request(url)
+            request.get_method = lambda: 'POST'
+            content = opener.open(request)
 
             self.response.headers['Content-Type'] = 'text/plain'
-            self.response.write(content)
+            self.response.write(content.read())
         else:
             self.error(400)
 
